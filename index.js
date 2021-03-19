59db69ab1cef9944ce59e5a7f83a850fde78ae70
Jesse Musa <jessemusa2@gmail.com>
2021-03-19 17:14:59 -0500

    changed port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 
 dotenv.config({path: './.env'});
 
-app.listen(5000, () => console.log("Listening at 7000"));
+app.listen(7000, () => console.log("Listening at 7000"));
 app.set("view-engine", 'ejs');
 app.use(express.json({limit: '1mb'}));
 app.use(express.urlencoded({extended: false}));
