39b1c8544abad7084ed42ac3388e7659d809b321
Jesse Musa <jessemusa2@gmail.com>
2021-03-19 17:25:53 -0500

    Added Tmiaur learn things

--- index.js
+++ index.js
@@ -12,3 +12,4 @@ app.use(express.json({limit: '1mb'}));
 app.use(express.urlencoded({extended: false}));
 
 app.use(express.static('public/LandingPage'));
+app.use("/Taimur",express.static('public/TaimurLearningPage'));
