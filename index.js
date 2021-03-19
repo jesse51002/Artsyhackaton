f6dddd257887c3f883b2ea2f646a76abced77ed7
Jesse Musa <jessemusa2@gmail.com>
2021-03-19 16:31:44 -0500

    base

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv');
+
+dotenv.config({path: './.env'});
+
+app.listen(5000, () => console.log("Listening at 7000"));
+app.set("view-engine", 'ejs');
+app.use(express.json({limit: '1mb'}));
+app.use(express.urlencoded({extended: false}));
+
+app.use(express.static('public/LandingPage'));
