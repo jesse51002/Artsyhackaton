dd8c652ae37498fd0ae4c5fd0973a74215ed733b
TaimurTariq2004 <80987044+TaimurTariq2004@users.noreply.github.com>
2021-03-19 17:19:11 -0500

    update

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv');
 
-const app = express();
-
 dotenv.config({path: './.env'});
 
-app.listen(5000, () => console.log("Listening at 7000"));
+app.listen(7000, () => console.log("Listening at 7000"));
 app.set("view-engine", 'ejs');
 app.use(express.json({limit: '1mb'}));
 app.use(express.urlencoded({extended: false}));
