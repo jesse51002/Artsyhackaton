45959554be1e862147f1050815229e4b9c081c47
Jesse Musa <jessemusa2@gmail.com>
2021-03-19 17:10:52 -0500

    app existes now

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv');
 
+const app = express();
+
 dotenv.config({path: './.env'});
 
 app.listen(5000, () => console.log("Listening at 7000"));
